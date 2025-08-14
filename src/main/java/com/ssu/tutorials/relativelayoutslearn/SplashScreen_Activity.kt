47c631ad4617a742e7b91b252953dcf8fbf59618
Sonu Kumar Singh <sonusinghsengar28@gmail.com>
2025-08-14 20:35:00 +0530

    Add files via upload

---
+++ src/main/java/com/ssu/tutorials/relativelayoutslearn/SplashScreen_Activity.kt
@@ -0,0 +1,24 @@
+package com.ssu.tutorials.relativelayoutslearn
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.delay
+
+class SplashScreen_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
